e5405a62ac757672d907179a830fdd463575a07c
user <user>
2022-08-12 00:06:38 -0400

    Prueba de obtencion de IP e Interface. Agregamos un ejemplo de comparacion de MAC leida vs MAC de inventario.

--- Trabajo3.py
+++ Trabajo3.py
@@ -1,2 +1,18 @@
-print("hola mundo")
+import requests
+import urllib3
+import json
+import conf
 
+urllib3.disable_warnings()
+url="https://192.168.100.215"
+
+r = requests.get(url+"/rest/ip/address/*3", auth=(conf.user,conf.clave), verify=False)
+#print(json.dumps(r.json(), indent=2))
+
+r2 = requests.get(url + "/rest/interface/ether1", auth=(conf.user,conf.clave), verify=False)
+#print(r2.json()["mac-address"])
+
+if r2.json()["mac-address"] == "00:0C:29:10:C7:72":
+    print("El equipo no fue cambiado")
+else:
+    print("Alguien cambio el equipo!!!")
