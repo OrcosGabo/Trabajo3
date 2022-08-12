7aa048b063a297cdbe92f87505a2220ab306e3c9
user <user>
2022-08-12 16:55:05 -0400

    Consumo 2, Metodo Bandwidth-test, con verbo POST.

--- Trabajo3.py
+++ Trabajo3.py
@@ -6,6 +6,6 @@ import conf
 urllib3.disable_warnings()
 url="https://192.168.100.215"
 
-r = requests.post(url+"/rest/interface/print", auth=(conf.user,conf.clave), verify=False)
+r = requests.post(url+"/rest/tool/bandwidth-test", auth=(conf.user,conf.clave), verify=False)
 print(json.dumps(r.json(), indent=2))
 
